d270ddb9246b3c73808d9fbf10874052e246474e
Rodrigo <rodrigo.sauceda@gmail.com>
2018-11-01 07:26:23 -0700

    actualizaciones

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromesasComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    let promesa = new Promise( (resolve, reject) => {
+
+      let contador = 0;
+
+      let intervalo = setInterval( () => {
+
+        contador += 1;
+
+        if ( contador === 3 ) {
+          resolve();
+          clearInterval(intervalo);
+        }
+
+      }, 1000 );
+
+    });
+
+    promesa.then(
+      () => console.log('Termino')
+    )
+    .catch( error => console.log('Error en la promesa', error));
+  }
 
   ngOnInit() {
   }
