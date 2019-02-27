56b79d7746acdb102888f383bf126151aa3c4a65
Rodrigo Sauceda <rodrigo.sauceda@gmail.com>
2019-02-27 08:46:23 -0800

    archivos

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -10,10 +10,8 @@ export class PromesasComponent implements OnInit {
   constructor() {
 
     let promesa = new Promise( (resolve, reject) => {
-
-      let contador = 0;
-
-      let intervalo = setInterval( () => {
+    let contador = 0;
+    let intervalo = setInterval( () => {
 
         contador += 1;
 
@@ -31,6 +29,44 @@ export class PromesasComponent implements OnInit {
     )
     .catch( error => console.log('Error en la promesa', error));
   }
+
+  onArchivoSeleccionado(input: HTMLInputElement) {
+
+    var files = input.files;
+    // console.log(files);
+    var len = files.length;
+    // console.log(len);
+    var arrayFilas:string[][] = [];
+
+    for (var t = 0; t < len; t++) {
+      var fr = new FileReader();
+      fr.onload = function(e) {
+        var text = fr.result;
+        var rows = text.split("\n");
+        // console.log(rows);
+
+          //Incorporo los valores de cada celda a arrayFilas.
+          for ( var i = 0; i < rows.length - 1; i++) {
+              if (rows[i].length!=0) {
+                  arrayFilas.push(rows[i].split("|"));
+              }
+            }
+            if (t=0) {
+              //Si es archivo 1
+              let muestras=arrayFilas.slice();
+              // console.log(rows);
+            } else if (t=1) {
+              //Si es archivo 2
+              let muestras2=arrayFilas.slice();
+              // console.log(rows);
+            }
+
+          };
+          fr.readAsText(files[t], 'ISO-8859-4');
+        }
+        console.log(arrayFilas);
+    }
+
 
   ngOnInit() {
   }
